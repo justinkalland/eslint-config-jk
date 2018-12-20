66ec0b7e757bd55b6aa6c40d29200329b2fc77d9
Justin Kalland <justin@kalland.ch>
2018-12-19 17:49:46 -0700

    Changed install script to modify package.json

--- scripts/postInstall.js
+++ scripts/postInstall.js
@@ -1,12 +1,42 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const path = require('path')
 
 const cwd = process.env.INIT_CWD || process.cwd()
+const packageFile = path.join(cwd, 'package.json')
 
-const file = `${cwd}/.eslintrc.json`
-const fileContents = '{"extends": "jk"}'
+if (!fs.existsSync(packageFile)) {
+  process.exit()
+}
 
-if (!fs.existsSync(file)) {
-  fs.writeFileSync(file, fileContents)
+const packageJson = require(packageFile)
+let packageChanged = false
+
+if (packageJson.husky === undefined) {
+  packageChanged = true
+  packageJson.husky = {
+    hooks: {
+      'pre-commit': 'lint-staged'
+    }
+  }
+}
+
+if (packageJson['lint-staged'] === undefined) {
+  packageChanged = true
+  packageJson['lint-staged'] = {
+    '*.js': 'eslint'
+  }
+}
+
+if (packageJson.eslintConfig === undefined) {
+  packageChanged = true
+  packageJson.eslintConfig = {
+    extends: 'jk'
+  }
+}
+
+if (packageChanged) {
+  const data = JSON.stringify(packageJson, null, 2) + '\n'
+  fs.writeFileSync(packageFile, data)
 }
